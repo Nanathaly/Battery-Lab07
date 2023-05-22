0bcc48ca763ed01eecf3877e6cc724794b9788c3
Nanathaly <marce.amao97@gmail.com>
2023-05-21 20:38:49 -0500

    Agregar archivos de proyecto.

---
+++ IBattery/IBattery/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace IBattery
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SetBackground(Battery.ChargeLevel, Battery.State == BatteryState.Charging);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+        }
+        protected override void OnDisappearing()
+        {  
+            base.OnDisappearing();
+            Battery.BatteryInfoChanged -= Battery_BatteryInfoChanged;
+        }
+
+        void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            SetBackground(e.ChargeLevel, e.State == BatteryState.Charging);
+        }
+        void SetBackground(double level, bool charging)
+        {
+            Color? color = null;
+            var status = charging ? "CARGANDO" : "NO ESTÁ CARGANDO";
+
+            if (level > .5f) 
+            {
+                color = Color.Green.MultiplyAlpha(level);
+            }
+            else if (level > .3f)
+            {
+                color = Color.Yellow.MultiplyAlpha(1d - level);
+            }
+            else
+            {
+                color = Color.Red.MultiplyAlpha(1d - level);
+            }
+            BackgroundColor = color.Value;
+            LabelBatteryLevel.Text = status;
+
+        }
+    }
+}
